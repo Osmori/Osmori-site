ad8e88f9afc47080a658916744fa0e36147f0605
Osmori <hello@osmori.com>
2026-03-08 22:02:28 -0400

    Update test-env.js

--- netlify/functions/test-env.js
+++ netlify/functions/test-env.js
@@ -2,9 +2,11 @@ exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      hasStripeKey: !!process.env.STRIPE_SECRET_KEY,
-      hasAirtableKey: !!process.env.AIRTABLE_API_KEY,
-      hasAirtableBase: !!process.env.AIRTABLE_BASE_ID,
+      message: "Test function working",
+      hasStripeKey: process.env.STRIPE_SECRET_KEY ? true : false,
+      hasAirtableKey: process.env.AIRTABLE_API_KEY ? true : false,
+      hasAirtableBase: process.env.AIRTABLE_BASE_ID ? true : false,
+      nodeVersion: process.version
     }),
   };
 };
