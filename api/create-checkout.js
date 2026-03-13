987bb897dfb68b979d4f3128d95561c37e393b02
Osmori <hello@osmori.com>
2026-03-12 22:06:19 -0400

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,20 +1,15 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 module.exports = async (req, res) => {
-  console.log('Function invoked with method:', req.method);
-  console.log('Request body:', req.body);
-
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
   try {
     const { priceId, customerEmail, customerName } = req.body;
-    console.log('Parsed data:', { priceId, customerEmail, customerName });
 
-    // Create the Stripe session with ui_mode: 'embedded'
     const session = await stripe.checkout.sessions.create({
-      ui_mode: 'embedded', // THIS is the magic line for embedded checkout
+      ui_mode: 'embedded',
       payment_method_types: ['card'],
       line_items: [{
         price: priceId,
@@ -25,11 +20,9 @@ module.exports = async (req, res) => {
       customer_email: customerEmail,
     });
 
-    console.log('Stripe session created:', session.id);
-    // We send back the clientSecret so the frontend can mount the form
     res.status(200).json({ clientSecret: session.client_secret });
   } catch (error) {
-    console.error('Error in function:', error);
+    console.error(error);
     res.status(500).json({ error: error.message });
   }
 };
